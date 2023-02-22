3178513c5f8c969d30b67a7cda3ee733f1d59a19
Erdi Rowlands <erdi.rowlands@harness.io>
2023-02-22 13:01:53 +0000

    FFM-6777 Update version

--- mix.exs
+++ mix.exs
@@ -21,7 +21,7 @@ defmodule Elixirsample.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      {:cfclient, github: "harness/ff-erlang-server-sdk", tag: "0.4.0-beta.3"}
+      {:cfclient, github: "harness/ff-erlang-server-sdk", tag: "0.5.0-beta.1"}
     ]
   end
 end
