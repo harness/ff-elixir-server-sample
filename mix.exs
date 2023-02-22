0c255066b55da4d8025f2c9fa4111f381c5362ba
Erdi Rowlands <erdi.rowlands@harness.io>
2023-02-22 12:45:58 +0000

    FFM-6777 Update sample app to use new TRR PR client init

--- mix.exs
+++ mix.exs
@@ -21,7 +21,7 @@ defmodule Elixirsample.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      {:cfclient, github: "harness/ff-erlang-server-sdk", branch: "main"}
+      {:cfclient, github: "harness/ff-erlang-server-sdk", tag: "0.4.0-beta.3"}
     ]
   end
 end
