af9b8aee90e4fe5b7aac0819b86911b83490c185
Erdi Rowlands <erdi.rowlands@harness.io>
2023-03-14 19:01:47 +0000

    FFM-7187 Tweak app for multi instance

--- config/config.exs
+++ config/config.exs
@@ -5,7 +5,7 @@ config :logger, :console, format: "[$level] $message\n"
 
 # Start up an instance of CFClient
 config :cfclient,
-       [api_key: System.get_env("YOUR_API_KEY"),
+       [api_key: System.get_env("HARNESS_API_KEY"),
         config: [config_url: "https://config.ff.harness.io/api/1.0"]]
 
 # For more complex applications where you need to use multiple Harness projects, you can start up additional
@@ -14,13 +14,13 @@ config :cfclient,
 # Instance for project 1
 config :elixirsample,  project1:
        [
-        api_key: System.get_env("PROJECT_1_API_KEY"),
+        api_key: System.get_env("HARNESS_PROJECT_1_KEY"),
         config: [name: :project1]
        ]
 
 # Instance for project 2
 config :elixirsample,  project2:
   [
-  api_key: System.get_env("PROJECT_2_API_KEY"),
+  api_key: System.get_env("HARNESS_PROJECT_2_KEY"),
   config: [name: :project2]
 ]
--- lib/elixir_sample/evaluation.ex
+++ lib/elixir_sample/evaluation.ex
@@ -10,7 +10,7 @@ defmodule ElixirSample.EvaluationSample do
     }
 
     # Default instance
-    flag = "harnessappdemodarkmode"
+    flag = "projectflag"
     result = :cfclient.bool_variation(flag, target, false)
 
     Logger.info(
@@ -18,16 +18,16 @@ defmodule ElixirSample.EvaluationSample do
     )
 
     # Instance 1
-    project_1_flag = "harnessappdemodarkmode"
-    project_1_result = :cfclient.bool_variation(:project1, project_1_flag, target, false)
+    project_1_flag = "project1flag"
+    project_1_result = :cfclient.number_variation(:project1, project_1_flag, target, 3)
 
     Logger.info(
       "SDK instance 1: Variation for Flag #{project_1_flag} with Target #{inspect(target)} is: #{project_1_result}"
     )
 
-    # Instance 1
-    project_2_flag = "harnessappdemodarkmodeproject2"
-    project_2_result = :cfclient.bool_variation(:project2, project_1_flag, target, false)
+    # Instance 2
+    project_2_flag = "project2flag"
+    project_2_result = :cfclient.bool_variation(:project2, project_2_flag, target, false)
 
     Logger.info(
       "SDK instance 2: Variation for Flag #{project_2_flag} with Target #{inspect(target)} is: #{project_2_result}"
