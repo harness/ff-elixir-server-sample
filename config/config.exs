a16dabd15ba626b2aca258dac67c1d329d30720d
Erdi Rowlands <erdi.rowlands@harness.io>
2023-03-14 14:56:36 +0000

    FFM-7187 Tweak config for multi instance

--- config/config.exs
+++ config/config.exs
@@ -1,3 +1,14 @@
 import Config
 
-config :cfclient, api_key: "YOUR_API_KEY"
+# Default instance TODO uncomment
+#config :cfclient, api_key: System.get_env("FF_API_KEY")
+
+# Named instance 1
+config :cfclient,
+       harness_project_1_config: [api_key: System.get_env("FF_API_KEY"),config: [name: "project1"]]
+
+#
+
+
+# Named instance 2
+# TODO
