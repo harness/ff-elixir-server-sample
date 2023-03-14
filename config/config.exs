5447cf77ca0197ceb19ec087159adcf559f52f19
Erdi Rowlands <erdi.rowlands@harness.io>
2023-03-14 19:17:02 +0000

    FFM-7187 Comment

--- config/config.exs
+++ config/config.exs
@@ -6,7 +6,7 @@ config :logger, :console, format: "[$level] $message\n"
 # Start up an instance of CFClient
 config :cfclient,
        [api_key: System.get_env("HARNESS_API_KEY"),
-       # For additional config you can pass in, see Erlang SDK docs https://github.com/harness/ff-elixir-server-sample/pull/4
+       # For additional config you can pass in, see Erlang SDK docs: https://github.com/harness/ff-erlang-server-sdk/blob/main/docs/further_reading.md#further-reading
        # we are just using the main config url here as an example.
         config: [config_url: "https://config.ff.harness.io/api/1.0"]]
 
