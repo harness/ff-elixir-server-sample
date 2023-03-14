4e8a77112adb149c7a5bc52e9525113e42f1cf8f
Erdi Rowlands <erdi.rowlands@harness.io>
2023-03-14 18:37:08 +0000

    FFM-7187 Tweak app for multi instance

--- config/config.exs
+++ config/config.exs
@@ -8,11 +8,38 @@ config :logger, :console, format: "[$level] $message\n"
 #       [api_key: System.get_env("FF_API_KEY"),
 #        config: [config_url: "https://config.ff.harness.io/api/1.0"]]
 
-config :elixirsample, :cfclient,
+#config :cfclient,  :elixirsample,
+#       [
+#         api_key: System.get_env("FF_API_KEY"),
+#         config: [name: :project1]
+#       ]
+
+config :elixirsample,  project1:
        [
-         api_key: System.get_env("FF_API_KEY"),
-         config: [name: :project1]
+        api_key: System.get_env("FF_API_KEY"),
+        config: [name: :project1]
        ]
+
+config :elixirsample,  project2:
+  [
+  api_key: System.get_env("FF_API_KEY_2"),
+  config: [name: :project2]
+]
+
+#config :elixirsample,  :cfclient,
+#       [
+#         api_key: System.get_env("FF_API_KEY"),
+#         project1_config: [config: [name: :project1]],
+#         project2_config: [config: [name: :project2]]
+#
+#       ]
+
+
+#config :elixirsample2,  :cfclient,
+#       [
+#         api_key: System.get_env("FF_API_KEY"),
+#         config: [name: :project12]
+#       ]
 
 
 ## Named instance 1
--- lib/elixir_sample/supervisor.ex
+++ lib/elixir_sample/supervisor.ex
@@ -10,17 +10,32 @@ defmodule ElixirSample.Supervisor do
 
   def init(_opts) do
 
-#    harness_args = Application.get_env(:cfclient, :harness_project_1_config, [])
+    project_1_config = Application.get_env(:elixirsample, :project1, [])
 
-    harness_args = Application.get_env(:elixirsample, :cfclient, [])
+    project_2_config = Application.get_env(:elixirsample, :project2, [])
+
 
     children = [
       %{
         id: :project1_cfclient_instance,
-        start: {:cfclient_instance, :start_link, [harness_args]},
+        start: {:cfclient_instance, :start_link, [project_1_config]},
+        type: :supervisor
+      },
+      %{
+        id: :project2_cfclient_instance,
+        start: {:cfclient_instance, :start_link, [project_2_config]},
         type: :supervisor
       },
     ]
+
+
+#    children = [
+##      %{
+##        id: :project1_cfclient_instance,
+##        start: {:cfclient_instance, :start_link, [harness_args]},
+##        type: :supervisor
+##      },
+#    ]
 
     Supervisor.init(children, strategy: :one_for_one)
   end
