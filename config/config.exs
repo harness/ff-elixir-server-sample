61ad9bd28b4c2175acdbea5a20f82f19cceaab93
Erdi Rowlands <erdi.rowlands@harness.io>
2023-03-14 18:44:15 +0000

    FFM-7187 Tweak app for multi instance

--- config/config.exs
+++ config/config.exs
@@ -3,22 +3,24 @@ import Config
 config :logger, :console, format: "[$level] $message\n"
 
 
-# Default instance
+# Start up an instance of CFClient
 config :cfclient,
-       [api_key: System.get_env("FF_API_KEY"),
+       [api_key: System.get_env("YOUR_API_KEY"),
         config: [config_url: "https://config.ff.harness.io/api/1.0"]]
 
+# For more complex applications where you need to use multiple Harness projects, you can start up additional
+# SDK instances for each project. 
 
 # Instance for project 1
 config :elixirsample,  project1:
        [
-        api_key: System.get_env("FF_API_KEY"),
+        api_key: System.get_env("PROJECT_1_API_KEY"),
         config: [name: :project1]
        ]
 
 # Instance for project 2
 config :elixirsample,  project2:
   [
-  api_key: System.get_env("FF_API_KEY_2"),
+  api_key: System.get_env("PROJECT_2_API_KEY"),
   config: [name: :project2]
 ]
